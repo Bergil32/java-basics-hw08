7cdd008506f8f9f3ba0ee8d38e11958efbaf7499
Dmytro Shavaryn <shavarynd@gmail.com>
2021-01-07 21:32:56 +0200

    hw 8 solution

--- src/RegExp.java
+++ src/RegExp.java
@@ -1,16 +1,26 @@
-/**
- * Regular expressions.
- */
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/** Regular expressions. */
 public class RegExp {
 
-    /**
-     * Get IP address from text.
-     *
-     * @param text text to parse
-     * @return String with IP address
-     */
-    public static String getIpAddress(String text) {
-        // TODO fill in code here for extracting IP address using Regular Expression
-        return "";
+  /**
+   * Get IP address from text.
+   *
+   * @param text text to parse
+   * @return String with IP address
+   */
+  public static String getIpAddress(String text) {
+    String regExp =
+        "\\b(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\b";
+
+    Pattern pattern = Pattern.compile(regExp);
+    Matcher matcher = pattern.matcher(text);
+
+    if (matcher.find()) {
+      return matcher.group();
     }
+
+    return "";
+  }
 }
